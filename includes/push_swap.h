19ac99a8cf475269eb4cf439d5c3ccf2ec5f9604
Mikel Garrido <mikelgarridolinares@gmail.com>
2025-03-26 18:55:10 +0100

    Initialization of the project

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: migarrid <migarrid@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 16:21:58 by migarrid          #+#    #+#             */
-/*   Updated: 2025/03/20 22:13:10 by migarrid         ###   ########.fr       */
+/*   Updated: 2025/03/26 18:35:40 by migarrid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,46 @@
 # include <limits.h>
 # include "./libft_plus/libft_plus.h"
 
-// ----------- Principal (Algortimo) ------------
-// -------------- Bonus (Checker) ---------------
+
 // -------------- Structure (Principal) ----------------
 typedef struct s_stack
 {
+	int	*stack;
+	int	size;
+	int	max_size;
+	int middle;
 }	t_stack;
+
+// ----------- Principal (Algortimo) ------------
+
+
+/*
+ *	Parser
+ */
+
+ void	ft_check_args(int ac, char **av);
+ int	*ft_parse_args(char **splitted, t_stack *stack);
+ char	**ft_process_args(int	ac, char **av, t_stack *stack);
+
+/*
+ *	Algorithm
+ */
+
+/*
+ *	Sort Utils
+ */
+
+/*
+ *	Exit Utils
+ */
+void	ft_exit(t_stack *stack_a, t_stack *stack_b);
+void	ft_error(char **splitted, t_stack *stack_a, t_stack *stack_b);
+void	ft_free_all_stacks(t_stack *stack_a, t_stack *stack_b);
+
+/*
+ *	Instructions
+ */
+
+// -------------- Bonus (Checker) ---------------
 
 #endif
--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: migarrid <migarrid@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 00:38:42 by migarrid          #+#    #+#             */
-/*   Updated: 2025/03/20 01:24:04 by migarrid         ###   ########.fr       */
+/*   Updated: 2025/03/26 18:38:03 by migarrid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,26 @@ int	main(int ac, char **av)
 {
 	t_stack	*stack_a;
 	t_stack	*stack_b;
+	char	**splitted;
 
 	if (ac == 1)
-		return (1);
-	return (0);
+		ft_exit(NULL, NULL);
+	ft_args_check(ac, av);
+	stack_a = ft_calloc(1, sizeof(t_stack));
+	stack_b = ft_calloc(1, sizeof(t_stack));
+	if (!stack_a || !stack_b)
+		ft_error(NULL, stack_a, stack_b);
+	splitted = ft_args_process(ac, av, stack_a);
+	if (!splitted)
+		ft_error(NULL, stack_a, stack_b);
+	stack_a->stack = ft_args_parser(splitted, stack_a);
+	if (!stack_a->stack)
+		ft_error(splitted, stack_a, stack_b);
+	stack_b->stack = ft_calloc(stack_a->size, sizeof(int));
+	if (!stack_b->stack)
+		ft_error (splitted, stack_a, stack_b);
+	stack_b->size = 0;
+	if (!ft_is_sorted(stack_a))
+		ft_init_sort(stack_a, stack_b);
+	return (ft_free_all_stacks(stack_a, stack_b), 0);
 }
