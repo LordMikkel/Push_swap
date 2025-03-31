83c678175f27ee728f1f55cef21ca468172a2dd6
LordMikkel <125906789+LordMikkel@users.noreply.github.com>
2025-03-31 11:18:52 +0200

    Init Sorting Algotirthm | Movements completed

---
+++ src/ft_instructions.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_instructions.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: migarrid <migarrid@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/29 23:47:10 by migarrid          #+#    #+#             */
+/*   Updated: 2025/03/30 00:28:00 by migarrid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/push_swap.h"
+
+void	ft_do_pa(t_stack *stack_a, t_stack *stack_b)
+{
+	int	tmp;
+
+	if (stack_b->size == 0)
+		return ;
+	stack_a->size += 1;
+	stack_b->size -= 1;
+	tmp = stack_b->stack[0];
+	ft_memmove(stack_b->stack, stack_b->stack + 1, stack_b->size * sizeof(int));
+	ft_memmove(stack_a->stack + 1, stack_a->stack, stack_a->size * sizeof(int));
+	stack_a->stack[0] = tmp;
+	ft_print_intructions("pa");
+}
+
+void	ft_do_pb(t_stack *stack_a, t_stack *stack_b)
+{
+	int	tmp;
+
+	if (stack_a->size == 0)
+		return ;
+	stack_a->size -= 1;
+	stack_b->size += 1;
+	tmp = stack_a->stack[0];
+	ft_memmove(stack_a->stack, stack_a->stack + 1, stack_a->size * sizeof(int));
+	ft_memmove(stack_b->stack + 1, stack_b->stack, stack_b->size * sizeof(int));
+	stack_b->stack[0] = tmp;
+	ft_print_instructions("pb");
+}
+
+void	ft_do_swap(t_stack *stack, char c)
+{
+	int	tmp;
+
+	if (stack->size < 2)
+		return ;
+	tmp = stack->stack[0];
+	stack->stack[0] = stack->stack[1];
+	stack->stack[1] = tmp;
+	if (c == 'a')
+		ft_print_intructions("sa");
+	else if (c == 'b')
+		ft_print_instructions("sb");
+}
+
+void	ft_do_rotate(t_stack *stack, char c)
+{
+	int	tmp;
+
+	if (stack->size < 2)
+		return ;
+	tmp = stack->stack[0];
+	ft_memmove(stack->stack, stack->stack + 1, stack->size * sizeof(int));
+	stack->stack[stack->size - 1] = tmp;
+	if (c == 'a')
+		ft_print_intructions("ra");
+	else if (c == 'b')
+		ft_print_instructions("rb");
+}
+
+void	ft_do_reverse_rotate(t_stack *stack, char c)
+{
+	int	tmp;
+
+	if (stack->size < 2)
+		return ;
+	tmp = stack->stack[stack->size - 1];
+	ft_memmove(stack->stack + 1, stack->stack, stack->size * sizeof(int));
+	stack->stack[0] = tmp;
+	if (c == "a")
+		ft_print_intructions("rra");
+	else if (c == "b")
+		ft_print_instructions("rrb");
+}
--- src/ft_sort_init.c
+++ src/ft_sort_init.c
@@ -6,12 +6,36 @@
 /*   By: migarrid <migarrid@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/29 18:47:16 by migarrid          #+#    #+#             */
-/*   Updated: 2025/03/29 18:52:30 by migarrid         ###   ########.fr       */
+/*   Updated: 2025/03/30 00:29:18 by migarrid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
+static void	ft_sort_two(t_stack *stack_a, t_stack *stack_b)
+{
+	ft_do_swap(stack_a, 'a');
+}
+
+static void	ft_sort_three(t_stack *stack_a, t_stack *stack_b)
+{
+}
+
+static void	ft_sort_five(t_stack *stack_a, t_stack *stack_b)
+{
+}
+
 void	ft_init_sort(t_stack *stack_a, t_stack *stack_b)
 {
+	if (stack_a->size == 2)
+		ft_sort_two(stack_a, stack_b);
+	else if (stack_a->size == 5)
+		ft_sort_three(stack_a, stack_b);
+	else if (stack_a->size <= 5)
+		ft_sort_five(stack_a, stack_b);
+	else if (stack_a->size <= 120)
+		ft_chunk_sort(stack_a, stack_b, 18);
+	else
+		ft_chunk_sort(stack_a, stack_b, 50);
+	ft_print_instructions("");
 }
