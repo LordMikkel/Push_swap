f1b24a525a33eb63faab6e009f392537d7f48814
LordMikkel <125906789+LordMikkel@users.noreply.github.com>
2025-04-02 18:00:11 +0200

    Algorithm works for 2 and 3 numbers

--- src/ft_instructions_printer.c
+++ src/ft_instructions_printer.c
@@ -1,17 +1,55 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_instructions.c                            :+:      :+:    :+:   */
+/*   ft_instructions_printer.c                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: migarrid <migarrid@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/29 21:42:16 by migarrid          #+#    #+#             */
-/*   Updated: 2025/03/29 21:43:51 by migarrid         ###   ########.fr       */
+/*   Updated: 2025/04/02 17:57:21 by migarrid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-void	ft_print_instruction(char *action)
+static void	ft_print_finish(char **last)
 {
+	ft_printf("%s\n", *last);
+	*last = NULL;
+}
+
+static void	ft_print_opt(char *opt, char **last)
+{
+	ft_printf("%s\n", opt);
+	*last = NULL;
+}
+
+static void	ft_print_update_last(char **last, char *action)
+{
+	ft_printf("%s\n", *last);
+	*last = action;
+}
+
+void	ft_print_instructions(char *action)
+{
+	static char	*last;
+
+	if (!action)
+		return (ft_print_finish(&last));
+	if (!last)
+		last = action;
+	else
+	{
+		if ((!ft_strcmp(last, "sa") && !ft_strcmp(action, "sb"))
+			|| (!ft_strcmp(last, "sb") && !ft_strcmp(action, "sa")))
+			return (ft_print_opt("ss", &last));
+		else if ((!ft_strcmp(last, "ra") && !ft_strcmp(action, "rb"))
+			|| (!ft_strcmp(last, "rb") && !ft_strcmp(action, "ra")))
+			return (ft_print_opt("rr", &last));
+		else if ((!ft_strcmp(last, "rra") && !ft_strcmp(action, "rrb"))
+			|| (!ft_strcmp(last, "rrb") && !ft_strcmp(action, "rra")))
+			return (ft_print_opt("rrr", &last));
+		else
+			return (ft_print_update_last(&last, action));
+	}
 }
--- src/ft_sort_init.c
+++ src/ft_sort_init.c
@@ -6,36 +6,54 @@
 /*   By: migarrid <migarrid@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/29 18:47:16 by migarrid          #+#    #+#             */
-/*   Updated: 2025/03/30 00:29:18 by migarrid         ###   ########.fr       */
+/*   Updated: 2025/04/02 17:59:31 by migarrid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-static void	ft_sort_two(t_stack *stack_a, t_stack *stack_b)
+static void	ft_sort_two(t_stack *stack)
 {
-	ft_do_swap(stack_a, 'a');
+	ft_do_swap(stack, 'a');
 }
 
-static void	ft_sort_three(t_stack *stack_a, t_stack *stack_b)
+static void	ft_sort_three(t_stack *stack)
 {
+	if (stack->stack[2] > stack->stack[1])
+	{
+		if (stack->stack[2] > stack->stack[0])
+			ft_do_swap(stack, 'a');
+		else
+			ft_do_rotate(stack, 'a');
+	}
+	if (stack->stack[0] > stack->stack[2])
+	{
+		if (stack->stack[0] > stack->stack[1])
+			ft_do_swap(stack, 'a');
+		ft_do_reverse_rotate(stack, 'a');
+	}
+	if (stack->stack[1] > stack->stack[2])
+	{
+		ft_do_swap(stack, 'a');
+		ft_do_rotate(stack, 'a');
+	}
 }
 
-static void	ft_sort_five(t_stack *stack_a, t_stack *stack_b)
+/*static void	ft_sort_five(t_stack *stack_a, t_stack *stack_b)
 {
-}
+}*/
 
 void	ft_init_sort(t_stack *stack_a, t_stack *stack_b)
 {
 	if (stack_a->size == 2)
-		ft_sort_two(stack_a, stack_b);
-	else if (stack_a->size == 5)
-		ft_sort_three(stack_a, stack_b);
+		ft_sort_two(stack_a);
+	else if (stack_a->size == 3)
+		ft_sort_three(stack_a);
 	else if (stack_a->size <= 5)
 		ft_sort_five(stack_a, stack_b);
-	else if (stack_a->size <= 120)
+	/*else if (stack_a->size <= 120)
 		ft_chunk_sort(stack_a, stack_b, 18);
 	else
-		ft_chunk_sort(stack_a, stack_b, 50);
-	ft_print_instructions("");
+		ft_chunk_sort(stack_a, stack_b, 50);*/
+	ft_print_instructions(NULL);
 }
